1ac2106765b6a6337ed8164f746c937197fff89b
sethleung19 <leung.sethc@gmail.com>
2026-07-17 01:40:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
 import smtplib
+import random
+import datetime as dt
+import pandas as pd
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+email = os.environ.get("sethleung0719@gmail.com")
+password = os.environ.get("qzfy vyef cycp rlwx")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+birthdays_data = pd.read_csv("birthdays.csv")
+letter_number = random.randint(1, 3)
+letter_name = "letter_templates/letter_" + str(letter_number) + ".txt"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+for row in birthdays_data.itertuples(index=True):
+    if row.day == dt.datetime.now().day and row.month == dt.datetime.now().month:
+
+        with open(letter_name) as file:
+            letter = file.read()
+            letter = letter.replace("[NAME]", row.name)
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as server:
+            server.starttls()
+            server.login(email, password)
+            server.sendmail(from_addr="sethleung0719@gmail.com",
+                            to_addrs=row.email,
+                            msg="".join(letter))
+
+
+
+
